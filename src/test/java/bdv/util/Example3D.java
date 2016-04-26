e7d94e493e1421b0de7b4d71b68d6e16f80eed5b
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-04-26 22:55:57 +0200

    update examples

---
+++ src/test/java/bdv/util/Example3D.java
@@ -0,0 +1,28 @@
+package bdv.util;
+
+import java.util.Random;
+
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.IntArray;
+import net.imglib2.type.numeric.ARGBType;
+
+public class Example3D
+{
+	public static void main( final String[] args )
+	{
+		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
+
+		final Random random = new Random();
+
+		final ArrayImg< ARGBType, IntArray > img = ArrayImgs.argbs( 100, 100, 100 );
+		img.forEach( t -> t.set( random.nextInt() & 0xFF00FF00 ) );
+		final Bdv bdv3D = BdvFunctions.show( img, "greens" );
+
+		final ArrayImg< ARGBType, IntArray > img2 = ArrayImgs.argbs( 100, 100, 100 );
+		img2.forEach( t -> t.set( random.nextInt() & 0xFFFF0000 ) );
+		BdvFunctions.show( img2, "reds", Bdv.options().addTo( bdv3D ) );
+
+		// TODO: add BdvOptions.closeAfterRemovingLastSource()
+	}
+}
--- src/test/java/bdv/util/PanelExample.java
+++ src/test/java/bdv/util/PanelExample.java
@@ -1,5 +1,7 @@
 package bdv.util;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -16,43 +18,18 @@ public class PanelExample
 	{
 		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
 
-		final ArrayImg< ARGBType, IntArray > img = ArrayImgs.argbs( 100, 100, 10 );
+		final ArrayImg< ARGBType, IntArray > img = ArrayImgs.argbs( 100, 100, 100 );
 		final Random random = new Random();
 		img.forEach( t -> t.set( random.nextInt() ) );
 
-
-		// TODO: add BdvOptions.closeAfterRemovingLastSource()
-
-
 		final JFrame frame = new JFrame( "my test frame" );
-
-
-		final BdvHandlePanel handle = new BdvHandlePanel(
-				frame,
-				Bdv.options().is2D() );
-		frame.add( handle.getViewerPanel() );
+		final BdvHandlePanel bdv = new BdvHandlePanel( frame, Bdv.options().is2D() );
+		frame.add( bdv.getViewerPanel(), BorderLayout.CENTER );
+		frame.setPreferredSize( new Dimension( 800, 600 ) );
 		frame.pack();
 		frame.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
 		frame.setVisible( true );
 
-
-		synchronized ( PanelExample.class )
-		{
-			try
-			{
-				final BdvSource source = BdvFunctions.show( img, "img", Bdv.options()
-						.addTo( handle )
-						.axisOrder( AxisOrder.XYC ) );
-				PanelExample.class.wait( 1000 );
-//				source.removeFromBdv();
-//				PanelExample.class.wait( 1000 );
-//				BdvFunctions.show( img, "img", Bdv.options().addTo( handle ) );
-			}
-			catch ( final InterruptedException e )
-			{
-				e.printStackTrace();
-			}
-		}
-
+		BdvFunctions.show( img, "img", Bdv.options().addTo( bdv ) );
 	}
 }
