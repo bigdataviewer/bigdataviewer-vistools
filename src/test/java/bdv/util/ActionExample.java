957e017fcc13b22eb937d42f864faf838dabd512
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-05-24 12:52:16 +0200

    Add examples for defining custom Actions and Behaviours

---
+++ src/test/java/bdv/util/ActionExample.java
@@ -0,0 +1,36 @@
+package bdv.util;
+
+import java.util.Random;
+import net.imglib2.RealPoint;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.IntArray;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.util.Util;
+import org.scijava.ui.behaviour.ClickBehaviour;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.util.Actions;
+import org.scijava.ui.behaviour.util.Behaviours;
+
+public class ActionExample
+{
+	public static void main( final String[] args )
+	{
+		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
+
+		final Random random = new Random();
+
+		final ArrayImg< ARGBType, IntArray > img = ArrayImgs.argbs( 100, 100, 100 );
+		img.forEach( t -> t.set( random.nextInt() & 0xFF00FF00 ) );
+		Bdv bdv = BdvFunctions.show( img, "greens", Bdv.options().is2D() );
+
+		Actions actions = new Actions( new InputTriggerConfig() );
+		actions.install( bdv.getBdvHandle().getKeybindings(), "my-new-actions" );
+
+		actions.runnableAction( () -> {
+				final RealPoint pos = new RealPoint( 3 );
+				bdv.getBdvHandle().getViewerPanel().getGlobalMouseCoordinates( pos );
+				System.out.println( "global coordinates: " + Util.printCoordinates(pos) );
+			}, "print global pos", "P" );
+	}
+}
---
+++ src/test/java/bdv/util/BehaviourExample.java
@@ -0,0 +1,41 @@
+package bdv.util;
+
+import java.util.Arrays;
+import java.util.Random;
+import net.imglib2.Point;
+import net.imglib2.RealPoint;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.IntArray;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.util.Util;
+import org.scijava.ui.behaviour.ClickBehaviour;
+import org.scijava.ui.behaviour.DragBehaviour;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.io.InputTriggerDescription;
+import org.scijava.ui.behaviour.util.Behaviours;
+
+public class BehaviourExample
+{
+	public static void main( final String[] args )
+	{
+		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
+
+		final Random random = new Random();
+
+		final ArrayImg< ARGBType, IntArray > img = ArrayImgs.argbs( 100, 100, 100 );
+		img.forEach( t -> t.set( random.nextInt() & 0xFF00FF00 ) );
+		Bdv bdv = BdvFunctions.show( img, "greens", Bdv.options().is2D() );
+
+		Behaviours behaviours = new Behaviours( new InputTriggerConfig() );
+		behaviours.install( bdv.getBdvHandle().getTriggerbindings(), "my-new-behaviours" );
+
+		behaviours.behaviour( ( ClickBehaviour ) ( x, y ) -> {
+			final RealPoint pos = new RealPoint( 3 );
+			bdv.getBdvHandle().getViewerPanel().displayToGlobalCoordinates( x, y, pos );
+			System.out.println( "global coordinates: " + Util.printCoordinates(pos) );
+		}, "print global pos", "P" );
+
+		// Note that you can also add DragBehaviours, ScrollBehaviours ...
+	}
+}
