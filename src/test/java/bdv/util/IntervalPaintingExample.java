f7f4aacde0f2b82500be4ad69fd2c02472b8d2e1
tpietzsch <tobias.pietzsch@gmail.com>
2021-01-15 17:57:24 +0100

    Bump to pom-scijava 30.0.0 and bigdataviewer-core 10.2.0

--- src/test/java/bdv/util/IntervalPaintingExample.java
+++ src/test/java/bdv/util/IntervalPaintingExample.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -52,8 +52,8 @@ import org.scijava.ui.behaviour.DragBehaviour;
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
 import org.scijava.ui.behaviour.util.Behaviours;
 
-import static net.imglib2.cache.img.DiskCachedCellImgOptions.CacheType.BOUNDED;
 import static net.imglib2.cache.img.DiskCachedCellImgOptions.options;
+import static net.imglib2.cache.img.optional.CacheOptions.CacheType.BOUNDED;
 
 /**
  * Shows a disk-cached checkerboard image.
