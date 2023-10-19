296feff5d2430b1353ef60a994ea35fc64def24e
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2023-10-19 14:56:25 -0400

    feat: introduce VolatileSource which relaxes the source type of       VolatileRandomAccessibleIntervalMipmapSource, deprecate that

--- src/main/java/bdv/util/VolatileSource.java
+++ src/main/java/bdv/util/VolatileSource.java
@@ -31,28 +31,61 @@ package bdv.util;
 import java.util.function.Supplier;
 
 import bdv.cache.SharedQueue;
+import bdv.util.volatiles.VolatileViews;
+import bdv.viewer.Source;
+import mpicbg.spim.data.sequence.VoxelDimensions;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.Volatile;
+import net.imglib2.cache.volatiles.CacheHints;
+import net.imglib2.cache.volatiles.LoadingStrategy;
+import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.NumericType;
 
-/**
- * @deprecated use {@code VolatileSource}
- */
-@Deprecated
-public class VolatileRandomAccessibleIntervalMipmapSource< T extends NumericType< T >, V extends Volatile< T > & NumericType< V > > extends VolatileSource< T, V >
+public class VolatileSource< T extends NumericType< T >, V extends Volatile< T > & NumericType< V > > extends AbstractSource< V >
 {
-	public VolatileRandomAccessibleIntervalMipmapSource(
-			final RandomAccessibleIntervalMipmapSource< T > source,
+	private final Source< T > source;
+
+	private SharedQueue queue;
+
+	public VolatileSource(
+			final Source< T > source,
 			final V type,
 			final SharedQueue queue )
 	{
-		super( source, type, queue );
+		super( type, source.getName() );
+		this.source = source;
+		this.queue = queue;
 	}
 
-	public VolatileRandomAccessibleIntervalMipmapSource(
-			final RandomAccessibleIntervalMipmapSource< T > source,
+	public VolatileSource(
+			final Source< T > source,
 			final Supplier< V > typeSupplier,
 			final SharedQueue queue )
 	{
-		super( source, typeSupplier, queue );
+		this( source, typeSupplier.get(), queue );
+	}
+
+	@Override
+	public RandomAccessibleInterval< V > getSource( final int t, final int level )
+	{
+		return VolatileViews.wrapAsVolatile( source.getSource( t, level ), queue, new CacheHints( LoadingStrategy.VOLATILE, level, true ) );
+	}
+
+	@Override
+	public synchronized void getSourceTransform( final int t, final int level, final AffineTransform3D transform )
+	{
+		source.getSourceTransform( t, level, transform );
+	}
+
+	@Override
+	public VoxelDimensions getVoxelDimensions()
+	{
+		return source.getVoxelDimensions();
+	}
+
+	@Override
+	public int getNumMipmapLevels()
+	{
+		return source.getNumMipmapLevels();
 	}
 }
